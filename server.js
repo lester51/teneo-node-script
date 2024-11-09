de5b51346436bcf9986f7e8c851e9acfa2a4d5a5
lester51 <lesternavarra@gmail.com>
2024-11-10 02:21:05 +0800

    added login

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'index.html'));
+  res.sendFile(path.join(__dirname, './public', 'login.html'));
 });
 
 app.listen(port, async() => {
